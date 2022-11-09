3aa8672a6416598aa7409848b988272688ea8fd2
Dhanuvnayak <dhanalakshmi.v@centelon.com>
2022-11-09 15:37:30 +0530

    package.json

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://dhanu:<password>@ac-wiybrru-shard-00-00.oycveuz.mongodb.net:27017,ac-wiybrru-shard-00-01.oycveuz.mongodb.net:27017,ac-wiybrru-shard-00-02.oycveuz.mongodb.net:27017/?ssl=true&replicaSet=atlas-i9nstt-shard-0&authSource=admin&retryWrites=true&w=majority", {
+mongoose.connect("mongodb://dhanu:lakshmi@ac-wiybrru-shard-00-00.oycveuz.mongodb.net:27017,ac-wiybrru-shard-00-01.oycveuz.mongodb.net:27017,ac-wiybrru-shard-00-02.oycveuz.mongodb.net:27017/?ssl=true&replicaSet=atlas-i9nstt-shard-0&authSource=admin&retryWrites=true&w=majority", {
     useNewUrlParser:true,
     useUnifiedTopology:true,
 
